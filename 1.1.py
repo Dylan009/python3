961fbc7f067cb688453c2aa4f3e37031dbcb3d2d
Dylan009 <andrewliufei@sina.cn>
2020-03-30 17:13:16 +0800

    第一次

---
+++ 1.1.py
@@ -0,0 +1,30 @@
+# _*_ coding: encoding utf-8 _*_
+from math import pi
+
+birth_year = input("birth year:")
+print(type(birth_year))
+age = 2019 - int(birth_year)
+print(type(age))
+print(age)
+
+print(pi)
+# print("\N{cat}")
+print("\u00c6")
+print("abcde".encode())
+print(ord("u"))
+
+print(pow(2, 3, 4))
+
+# abs(number)
+# bytes(string, encoding[, errors])
+# cmath.sqrt(number)
+# float(object)
+# help(object)
+# input(object)
+# int(object)
+# math.ceil(number)  以浮点数的方式返回向上取整的结果
+# math.floor(number)  以浮点数的方式返回向下取整的结果
+# math.sqrt(number)
+# pow(x, y[, z])
+# print(object, ...)
+# repr(object)  返回指定值的字符串表示
